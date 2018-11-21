389dfa46e0344ea7709bacd9d8df25254ba2ff94
sumits1900 <45158031+sumits1900@users.noreply.github.com>
2018-11-21 17:32:48 -0500

    Create nodejs.groovy

---
+++ dsl-test/nodejs.groovy
@@ -0,0 +1,18 @@
+job('NodeJS example') {
+    scm {
+        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('Sumit')
+            node / gitConfigEmail('sumit1900@gmail.com')
+        }
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs') // this is the name of the NodeJS installation in 
+                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
+    }
+    steps {
+        shell("npm install")
+    }
+}
